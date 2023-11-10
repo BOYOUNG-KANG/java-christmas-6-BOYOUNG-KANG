ecdc5f047475145c6b41fc0a0d03d2fae6a955a7
BOYOUNG-KANG <28287@naver.com>
2023-11-10 10:45:28 +0900

    feat(Date): 주말 할인 계산 로직 추가

---
+++ src/main/java/christmas/domain/Date.java
@@ -0,0 +1,22 @@
+package christmas.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.EnumMap;
+
+public class Date {
+    private static final int DISCOUNT_PRICE = 2023;
+
+    private LocalDate date;
+
+    public Date(int date) {
+        int discount = 0;
+        this.date = LocalDate.of(2023, 12, date);
+    }
+    private void dayOfWeekDiscount(int discount, EnumMap<Category, Integer> orderInfo){
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        if (dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)){
+            discount += orderInfo.get(Category.MAIN) * DISCOUNT_PRICE;
+        }
+    }
+}
