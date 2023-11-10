e29454681aee6397534aea995c60d1fce04021ae
BOYOUNG-KANG <28287@naver.com>
2023-11-10 17:02:01 +0900

    feat(Date): 크리스마스 할인 계산 로직 추가

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -6,15 +6,16 @@ import java.util.EnumMap;
 
 public class Date {
     private static final int DISCOUNT_PRICE = 2023;
-    private static final int SPECIAL_DISCOUNT_PRICE = 1000;
     private static final int CHRISTMAS_DATE = 25;
     private LocalDate date;
 
     public Date(int date) {
-        int discount = 0;
         this.date = LocalDate.of(2023, 12, date);
     }
-    private void dayOfWeekDiscount(int discount, EnumMap<Category, Integer> orderInfo){
+    public int discount(int discount, EnumMap<Category, Integer> orderInfo){
+        return dayOfWeekDiscount(discount, orderInfo);
+    }
+    private int dayOfWeekDiscount(int discount, EnumMap<Category, Integer> orderInfo){
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         if (dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)){
             discount += orderInfo.get(Category.MAIN) * DISCOUNT_PRICE;
@@ -22,12 +23,20 @@ public class Date {
         if (!dayOfWeek.equals(DayOfWeek.FRIDAY) && !dayOfWeek.equals(DayOfWeek.SATURDAY)){
             discount += orderInfo.get(Category.DESSERT) * DISCOUNT_PRICE;
         }
+        return specialDiscount(discount);
     }
-    private void specialDiscount(int discount){
+    private int specialDiscount(int discount){
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         if (dayOfWeek.equals(DayOfWeek.SUNDAY) || date.getDayOfMonth() == CHRISTMAS_DATE){
-            discount += SPECIAL_DISCOUNT_PRICE;
+            discount += 1000;
         }
+        return christmasDiscount(discount);
     }
-
+    private int christmasDiscount(int discount){
+        int dayOfMonth = date.getDayOfMonth();
+        if (dayOfMonth <= CHRISTMAS_DATE) {
+            discount += (1000 + 100 * (dayOfMonth - 1));
+        }
+        return discount;
+    }
 }
