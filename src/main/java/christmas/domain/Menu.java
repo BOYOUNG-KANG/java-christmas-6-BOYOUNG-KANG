9f5fd705fc5660d9607cf733f14b8c507f5f23da
BOYOUNG-KANG <28287@naver.com>
2023-11-12 14:57:16 +0900

    feat(InputView): 주문 메뉴 입력 받기 로직 추가

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,24 +1,30 @@
 package christmas.domain;
 
 public enum Menu {
-    MUSHROOM_SOUP(Category.APPETIZER, 6000),
-    TAPAS(Category.APPETIZER,5500),
-    CAESAR_SALAD(Category.APPETIZER,8000),
-    T_BONE_STEAK(Category.MAIN,55000),
-    BBQ_RIBS(Category.MAIN,54000),
-    SEAFOOD_PASTA(Category.MAIN,35000),
-    CHRISTMAS_PASTA(Category.MAIN,25000),
-    CHOCO_CAKE(Category.DESSERT,15000),
-    ICE_CREAM(Category.DESSERT,5000),
-    ZERO_COKE(Category.DRINK,3000),
-    RED_WINE(Category.DRINK,60000),
-    CHAMPAGNE(Category.DRINK,25000);
+    MUSHROOM_SOUP("양송이스프", Category.APPETIZER, 6000),
+    TAPAS("타파스", Category.APPETIZER,5500),
+    CAESAR_SALAD("시저샐러드", Category.APPETIZER,8000),
+    T_BONE_STEAK("티본스테이크", Category.MAIN,55000),
+    BBQ_RIBS("바비큐립", Category.MAIN,54000),
+    SEAFOOD_PASTA("해산물파스타", Category.MAIN,35000),
+    CHRISTMAS_PASTA("크리스마스파스타", Category.MAIN,25000),
+    CHOCO_CAKE("초코케이크", Category.DESSERT,15000),
+    ICE_CREAM("아이스크림", Category.DESSERT,5000),
+    ZERO_COKE("제로콜라", Category.DRINK,3000),
+    RED_WINE("레드와인", Category.DRINK,60000),
+    CHAMPAGNE("샴페인", Category.DRINK,25000);
+    private String name;
     private Category category;
     private int price;
 
-    Menu(Category category, int price) {
+    Menu(String name, Category category, int price) {
+        this.name = name;
         this.category = category;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Category getCategory() {
