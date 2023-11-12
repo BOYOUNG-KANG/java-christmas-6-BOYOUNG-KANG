ac8c667411ac09253b105bb4344bc486d7aaffb9
BOYOUNG-KANG <28287@naver.com>
2023-11-12 16:22:33 +0900

    fix(Date): 메뉴 갯수별 할인 로직 추가

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -3,6 +3,7 @@ package christmas.domain;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.EnumMap;
+import java.util.Map;
 
 public class Date {
     private static final int DISCOUNT_PRICE = 2023;
@@ -12,18 +13,24 @@ public class Date {
     public Date(int date) {
         this.date = LocalDate.of(2023, 12, date);
     }
-    public int discount(int discount, EnumMap<Category, Integer> orderInfo){
+    public int discount(int discount, EnumMap<Menu, Integer> orderInfo){
         return dayOfWeekDiscount(discount, orderInfo);
     }
-    private int dayOfWeekDiscount(int discount, EnumMap<Category, Integer> orderInfo){
+    private int dayOfWeekDiscount(int discount, EnumMap<Menu, Integer> orderInfo){
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         if (dayOfWeek.equals(DayOfWeek.FRIDAY) || dayOfWeek.equals(DayOfWeek.SATURDAY)){
-            discount += orderInfo.get(Category.MAIN) * DISCOUNT_PRICE;
+            discount += countBenefittedMenus(orderInfo, Category.MAIN) * DISCOUNT_PRICE;
         }
         if (!dayOfWeek.equals(DayOfWeek.FRIDAY) && !dayOfWeek.equals(DayOfWeek.SATURDAY)){
-            discount += orderInfo.get(Category.DESSERT) * DISCOUNT_PRICE;
+            discount += countBenefittedMenus(orderInfo, Category.DESSERT) * DISCOUNT_PRICE;
         }
         return specialDiscount(discount);
+    }
+    private int countBenefittedMenus(EnumMap<Menu, Integer> orderInfo, Category category){
+        return orderInfo.entrySet().stream()
+                .filter(entry -> entry.getKey().getCategory() == category)
+                .mapToInt(Map.Entry::getValue)
+                .sum();
     }
     private int specialDiscount(int discount){
         DayOfWeek dayOfWeek = date.getDayOfWeek();
