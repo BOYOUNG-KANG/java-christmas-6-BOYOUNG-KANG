71ce5d1519437acb8bc4892330a79175c5f446bf
BOYOUNG-KANG <28287@naver.com>
2023-11-13 23:59:34 +0900

    feat(Badge): 배지 생성 및 배지 부여 로직 추가

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,31 @@
+package christmas.domain;
+
+public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000),
+    NOTHING("없음", 0);
+    private String name;
+    private int discount;
+
+    Badge(String name, int discount) {
+        this.name = name;
+        this.discount = discount;
+    }
+    public static Badge get(int totalDiscount){
+        if (totalDiscount >= SANTA.discount) {
+            return SANTA;
+        }
+        if (totalDiscount >= TREE.discount) {
+            return TREE;
+        }
+        if (totalDiscount >= STAR.discount) {
+            return STAR;
+        }
+        return NOTHING;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
